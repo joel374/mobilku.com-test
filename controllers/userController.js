fbe6a47804a87cfa98304938c45b854f2dcf3257
Joel Legifani Manisali <joellegifanimanisali@gmail.com>
2023-03-28 09:52:39 +0700

    try

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,36 +1,36 @@
-const sharp = require("sharp")
-const db = require("../models")
+const sharp = require("sharp");
+const db = require("../models");
 
 const userController = {
   view: async (req, res) => {
     try {
-      const result = await db.User.findAll()
+      const result = await db.User.findAll();
 
       return res.status(200).json({
         message: "Find All Users",
         data: result,
-      })
+      });
     } catch (error) {
-      console.log(error)
+      console.log(error);
       return res.status(500).json({
         message: "Server Error",
-      })
+      });
     }
   },
   create: async (req, res) => {
     try {
-      const { name, mobile, education, usia } = req.body
+      const { name, mobile, education, usia } = req.body;
       if (req.file) {
-        const image = req.file.filename
-        const imageSplit = image.split(".")
+        const image = req.file.filename;
+        const imageSplit = image.split(".");
 
         await sharp(`./public/${req.file.filename}`)
           .resize(500, 500)
-          .toFile(`./public/IMAGE1-${imageSplit[0]}.${imageSplit[1]}`)
+          .toFile(`./public/IMAGE1-${imageSplit[0]}.${imageSplit[1]}`);
 
         await sharp(`./public/${req.file.filename}`)
           .resize(1000, 1000)
-          .toFile(`./public/IMAGE2-${imageSplit[0]}.${imageSplit[1]}`)
+          .toFile(`./public/IMAGE2-${imageSplit[0]}.${imageSplit[1]}`);
       }
 
       const result = await db.User.create({
@@ -40,45 +40,47 @@ const userController = {
         usia,
         image1: req.file ? `IMAGE1-${imageSplit[0]}.${imageSplit[1]}` : null,
         image2: req.file ? `IMAGE2-${imageSplit[0]}.${imageSplit[1]}` : null,
-      })
+      });
 
       return res.status(200).json({
         message: "User created successfully",
         data: result,
-      })
+      });
     } catch (error) {
-      console.log(error)
+      console.log(error);
       return res.status(500).json({
         message: "Server Error",
-      })
+      });
     }
   },
   update: async (req, res) => {
     try {
-      const { id } = req.params
+      const { id } = req.params;
 
-      const image = req.file.filename
-      const imageSplit = image.split(".")
-      const { name, mobile, education, usia } = req.body
+      const { name, mobile, education, usia } = req.body;
+      if (req.file) {
+        const image = req.file.filename;
+        const imageSplit = image.split(".");
 
-      await sharp(`./public/${req.file.filename}`)
-        .resize(500, 500)
-        .toFile(`./public/IMAGE1-${imageSplit[0]}.${imageSplit[1]}`)
+        await sharp(`./public/${req.file.filename}`)
+          .resize(500, 500)
+          .toFile(`./public/IMAGE1-${imageSplit[0]}.${imageSplit[1]}`);
 
-      await sharp(`./public/${req.file.filename}`)
-        .resize(1000, 1000)
-        .toFile(`./public/IMAGE2-${imageSplit[0]}.${imageSplit[1]}`)
+        await sharp(`./public/${req.file.filename}`)
+          .resize(1000, 1000)
+          .toFile(`./public/IMAGE2-${imageSplit[0]}.${imageSplit[1]}`);
+      }
 
       const findUser = await db.User.findOne({
         where: {
           id: id,
         },
-      })
+      });
 
       if (!findUser) {
         return res.status(404).json({
           message: "User not found",
-        })
+        });
       }
 
       await db.User.update(
@@ -87,26 +89,26 @@ const userController = {
           mobile,
           education,
           usia,
-          image1: `IMAGE1-${imageSplit[0]}.${imageSplit[1]}`,
-          image2: `IMAGE2-${imageSplit[0]}.${imageSplit[1]}`,
+          image1: req.file ? `IMAGE1-${imageSplit[0]}.${imageSplit[1]}` : null,
+          image2: req.file ? `IMAGE2-${imageSplit[0]}.${imageSplit[1]}` : null,
         },
         {
           where: {
             id: id,
           },
         }
-      )
+      );
 
       return res.status(200).json({
         message: "User updated successfully",
-      })
+      });
     } catch (error) {
-      console.log(error)
+      console.log(error);
       return res.status(500).json({
         message: "Server Error",
-      })
+      });
     }
   },
-}
+};
 
-module.exports = userController
+module.exports = userController;
--- index.js
+++ index.js
@@ -1,32 +1,28 @@
-const express = require("express")
-const app = express()
-const port = 3000
-const db = require("./models")
-const userRouter = require("./routers/userRouter")
-const fs = require("fs")
+const express = require("express");
+const app = express();
+const port = 3000;
+const db = require("./models");
+const userRouter = require("./routers/userRouter");
+const fs = require("fs");
 
-app.use(express.json())
-app.use("/user", userRouter)
+app.use(express.json());
+app.use("/user", userRouter);
 
 app.get("/", (req, res) => {
-  res.send("Hello World!")
-})
-const config = JSON.parse(fs.readFileSync("./config/config.json"))
-
-// app.use(express.static(__dirname + "/public"))
-// app.use("/public", express.static("public"))
+  res.send("Hello World!");
+});
 
 app.listen(port, (err) => {
   if (err) {
-    console.log("ERROR: ".concat(err))
+    console.log("ERROR: ".concat(err));
   } else {
     db.sequelize.sync({
       alter: true,
-    })
+    });
     // if (!fs.existsSync("public")) {
     //   // fs.mkdirSync("public")
     //   fs.mkdtemp("./temp")
     // }
-    console.log("APP RUNNING at ".concat(port, " \u2705"))
+    console.log("APP RUNNING at ".concat(port, " \u2705"));
   }
-})
+});
